84897580ea5795ae1f886f8ae5b992c0b0e3b728
Yeison Stiven Calle Londoño <yeiksite2@gmail.com>
2014-12-17 09:22:47 -0500

    Start has begun

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 
-	var disqus_shortname = 'yeiksite';
-	(function() {
-		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	})();
+    var disqus_shortname = 'yeiksite2'; 
+     
+   (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
 
-	$('.comments').html('<div id="disqus_thread"></div><noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript><a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>');
 
+	$('.comments').html('<div id="disqus_thread"></div> <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>');
+
+	$('.blur').blurjs({
+	    source: 'body',
+	    radius: 10
+	});
 });
+
+        
+    
