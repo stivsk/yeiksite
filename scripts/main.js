572aba3866275fe35f9b4fbad1563472dea73066
Yeison Stiven Calle Londoño <yeiksite2@gmail.com>
2014-12-29 10:04:59 -0500

    Backgrund Slide

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,13 @@
 $(document).ready(function(){
 
-    var disqus_shortname = 'yeiksite2'; 
-     
-   (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-
-
-	$('.comments').html('<div id="disqus_thread"></div> <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>');
-
-	$('.blur').blurjs({
-	    source: 'body',
-	    radius: 40
-	});
+ $.backstretch([
+      "http://dl.dropbox.com/u/515046/www/outside.jpg"
+    , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
+    , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+  ], {duration: 3000, fade: 750});
 
 ////////////////////////////////////////////////////////////////////////////////
 var cursos = ['javaScript','html5', 'ruby on rails','java','perl', 'php','ruby','Django','NodeJs'];
-
 
     function displayCourses(arr){
         var out = "";
@@ -29,22 +18,16 @@ var cursos = ['javaScript','html5', 'ruby on rails','java','perl', 'php','ruby',
         }
         document.getElementById('courses').innerHTML = out;
     }
-
     displayCourses(cursos);
 ////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
+    var disqus_shortname = 'yeiksite2'; 
+   (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+	$('.comments').html('<div id="disqus_thread"></div> <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>');
 
     
 });
